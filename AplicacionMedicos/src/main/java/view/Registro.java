f1fd47792ab70512462db8aa70eee122ddfd8b79
Sebastian <samesco323@gmail.com>
2024-04-26 16:05:41 +0200

    Registro

--- AplicacionMedicos/src/main/java/view/Registro.java
+++ AplicacionMedicos/src/main/java/view/Registro.java
@@ -1,17 +1,29 @@
 package view;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
 import java.awt.Font;
+import java.text.ParseException;
+
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.MaskFormatter;
 
 public class Registro extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JLabel labelPaswd;
+	private MaskFormatter mask;
 
 	/**
 	 * Launch the application.
@@ -48,27 +60,74 @@ public class Registro extends JFrame {
 		
 		JLabel name = new JLabel("Nombre:");
 		name.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		name.setBounds(288, 90, 56, 21);
+		name.setBounds(73, 132, 56, 21);
 		contentPane.add(name);
 		
 		JLabel surname = new JLabel("Apellidos:");
 		surname.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		surname.setBounds(100, 127, 56, 31);
+		surname.setBounds(310, 127, 56, 31);
 		contentPane.add(surname);
 		
 		JLabel dob = new JLabel("Fecha de nacimiento:");
 		dob.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		dob.setBounds(100, 181, 145, 21);
+		dob.setBounds(7, 180, 145, 21);
 		contentPane.add(dob);
 		
 		JLabel gender = new JLabel("Sexo:");
 		gender.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		gender.setBounds(100, 238, 45, 13);
+		gender.setBounds(329, 182, 30, 17);
 		contentPane.add(gender);
 		
 		JLabel dni = new JLabel("DNI:");
 		dni.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		dni.setBounds(100, 93, 45, 13);
+		dni.setBounds(87, 93, 45, 13);
 		contentPane.add(dni);
+		
+		textField = new JTextField();
+		textField.setBounds(139, 92, 135, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(376, 134, 135, 19);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(139, 134, 135, 19);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(376, 92, 135, 19);
+		contentPane.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(376, 182, 135, 19);
+		contentPane.add(textField_5);
+		
+		labelPaswd = new JLabel("Contraseña:");
+		labelPaswd.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		labelPaswd.setBounds(300, 94, 79, 13);
+		contentPane.add(labelPaswd);
+		
+		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnNewButton.setBounds(391, 295, 120, 44);
+		contentPane.add(btnNewButton);
+		
+		JFormattedTextField formattedTextField = new JFormattedTextField();
+		formattedTextField.setBounds(142, 182, 135, 21);
+		contentPane.add(formattedTextField);
+		try {
+			mask = new MaskFormatter("##/##/####");
+			mask.setValidCharacters("0123456789");
+			formattedTextField= new JFormattedTextField(mask);
+			
+		}catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
